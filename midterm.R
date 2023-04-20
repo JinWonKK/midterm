ac13f0450825caa6d6f321a65e87eedaa31ecb9d
JinWonKK <103561340+JinWonKK@users.noreply.github.com>
2023-04-20 21:50:34 +0900

    commit입니다.

--- midterm.R
+++ midterm.R
@@ -1,3 +1,8 @@
-foo <- USArrests[1:5,]
+foo <- USArrests[1:50,]
 bar <- USArrests[,c("Assault")]
-foobar <- 
+foobar <- data.frame(foo,bar)
+wordcloud(words = foobar[1:50,], freq = foobar[,1:50])
+sink('foobar.csv', append = T)
+cat(foobar)
+sink()
+rm(list = ls())
